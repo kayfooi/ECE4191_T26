f1457cb5184a0ba8d7dd5159d3c5ff6d8588f741
kayfooi <isabellehee128@gmail.com>
2024-08-15 08:53:23 +1000

    created arduino stuff

---
+++ arduino_stuff/sketch_aug15a/sketch_aug15a.ino
@@ -0,0 +1,19 @@
+// Encoder stuff
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 200
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// Ultrasonic Sensor stuff
