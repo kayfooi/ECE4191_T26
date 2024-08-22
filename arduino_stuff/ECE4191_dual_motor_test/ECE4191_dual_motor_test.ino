6ad51fe025887ddf8e0250e42ad626ed89c9d4a0
rohitphalkeee <rjphalke@gmail.com>
2024-08-22 10:54:53 +1000

    Update ECE4191_dual_motor_test.ino

--- arduino_stuff/ECE4191_dual_motor_test/ECE4191_dual_motor_test.ino
+++ arduino_stuff/ECE4191_dual_motor_test/ECE4191_dual_motor_test.ino
@@ -2,21 +2,21 @@
 // Might need PWM pins for speed
 // Pins 2-13 and 44-46
 // NAME THESE MOTORS LEFT AND RIGHT EVENTUALLY
-int motor1dir = 31;
-int motor1speed = 30;
-int motor2dir = 33;
-int motor2speed = 32;
+int motorRightdir = 30;
+int motorRightspeed = 4;
+int motorLeftdir = 31;
+int motorLeftspeed = 5;
 
 // define encoder pins for motors
 // Interrupt pins are 2-3 and 18-21
-int encoder1 = 2;
-int encoder1in = 18;
-int encoder2 = 3;
-int encoder2in = 19;
+int encoderRight = 2;
+int encoderRightin = 18;
+int encoderLeft = 3;
+int encoderLeftin = 19;
 
 // define variables for encoder counts
-volatile long encoder1Count = 0;
-volatile long encoder2Count = 0;
+volatile long encoderRightCount = 0;
+volatile long encoderLeftCount = 0;
 
 // define variables for PID control
 long previousTime = 0;
@@ -25,16 +25,22 @@ float eIntegral = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motor1dir, OUTPUT);
-  pinMode(motor1speed, OUTPUT);
-  pinMode(motor2dir, OUTPUT);
-  pinMode(motor2speed, OUTPUT);
-  pinMode(encoder1, OUTPUT);
-  pinMode(encoder2, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("serial test");
+
+  pinMode(motorRightdir, OUTPUT);
+  pinMode(motorRightspeed, OUTPUT);
+  pinMode(motorLeftdir, OUTPUT);
+  pinMode(motorLeftspeed, OUTPUT);
+  pinMode(encoderRight, INPUT);
+  pinMode(encoderRightin, INPUT);
+  pinMode(encoderLeft, INPUT);
+  pinMode(encoderLeftin, INPUT);
 
   // Interrupts
-  attachInterrupt(digitalPinToInterrupt(encoder1), handleEncoder1, RISING);
-  attachInterrupt(digitalPinToInterrupt(encoder2), handleEncoder2, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderRight), handleEncoder1, RISING);
+  attachInterrupt(digitalPinToInterrupt(encoderLeft), handleEncoder2, RISING);
+
 
 }
 
@@ -55,11 +61,14 @@ void loop() {
   */
 
   // Set desired setpoint for motor 2
-  int target = encoder1Count;
+  int target = encoderRightCount;
 
   // Move motor 1
-  digitalWrite(motor1dir, 1);
-  analogWrite(motor1speed, 50);
+  //digitalWrite(motorRightdir, 0);
+  //analogWrite(motorRightspeed, 50);
+
+  digitalWrite(motorRightdir, 0);
+  analogWrite(motorRightspeed, 255);
   
   // PID gains and computation
   float kp = 2.0;
@@ -68,22 +77,29 @@ void loop() {
   float u = pidController(target, kp, kd, ki);
 
   // Control motor 2 based on PID
-  moveMotor(motor2dir, motor2speed, u);
+  moveMotor(motorLeftdir, motorLeftspeed, u);
 
   // Print statements for debugging
-  Serial.print(encoder1Count);
+  Serial.print(encoderRightCount);
   Serial.print(", ");
-  Serial.println(encoder2Count);
+  Serial.println(encoderLeftCount);
+  delay(3000);
+
+  analogWrite(motorRightspeed, 0);
+  analogWrite(motorLeftspeed, 0);
+
+  while (true);
+
 
 }
 
 // Functions called during interrupts
 
 void handleEncoder1() {
-  encoder1Count++;
+  encoderRightCount++;
 }
 void handleEncoder2() {
-  encoder2Count++;
+  encoderLeftCount++;
 }
 
 void moveMotor(int dirPin, int pwmPin, float u) {
@@ -93,11 +109,11 @@ void moveMotor(int dirPin, int pwmPin, float u) {
     speed = 255;
   }
   // Stop the motor during overshoot
-  if (encoder2Count > encoder1Count) {
+  if (encoderLeftCount > encoderRightCount) {
     speed = 0;
   }
   // Control the motor
-  int direction = 0;
+  int direction = 1; // direction set to 1 because right motor mounted in reverse
   digitalWrite(dirPin, direction);
   analogWrite(pwmPin, speed);
 }
@@ -108,7 +124,7 @@ float pidController(int target, float kp, float kd, float ki) {
   float deltaT = ((float)(currentTime - previousTime))/1.0e6;
 
   // Compute the error, derivative and integral
-  int e = encoder2Count - target;
+  int e = encoderLeftCount - target;
   float eDerivative = (e - ePrevious)/deltaT;
   eIntegral = eIntegral + e*deltaT;
 
