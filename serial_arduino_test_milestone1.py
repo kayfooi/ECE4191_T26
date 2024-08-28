d3f9a8b9613746c3c596f5b332602dc064baaffb
Aryaman <akat0014@student.monash.edu>
2024-08-29 04:37:41 +1000

    4.37

--- serial_arduino_test_milestone1.py
+++ serial_arduino_test_milestone1.py
@@ -9,3 +9,7 @@ while True:
     ser.write(b'T_100\n')
     time.sleep(3) #sleep time in seconds 
     ser.write(b'R_-50\n')
+    time.sleep(3)
+    ser.write(b'T_-100\n')
+    time.sleep(3) #sleep time in seconds 
+    ser.write(b'R_50\n')
