8c6de948e67daa4ed55822400b061b4cce89c5bc
kayfooi <isabellehee128@gmail.com>
2024-09-10 15:22:22 +1000

    updated infrared.py
    used pigpio library for IR sensors

--- rPi_sensor/infrared.py
+++ rPi_sensor/infrared.py
@@ -1,6 +1,21 @@
 from time import sleep
 import machine
 
+#with rpigpio library
+import pigpio
+
+IR_in = 11 #pin number signal
+IR_out = 13 #pin number LED
+
+pi.set_mode(IR_in, pigpio.INPUT)
+pi.set_mode(IR_out, pigpio.OUTPUT)
+
+IR_signal = pi.read(IR_in)
+pi.write(IR_out, 1) #sets LED high
+
+
+
+
 #can adjust potentiometer to adjust sensitivity
 #can be used with any microcontroller (rPi or arduino)
 
